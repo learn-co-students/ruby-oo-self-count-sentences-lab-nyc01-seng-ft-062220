45a3e07c264c219da29c685e7055e1d25ec0a55a
github username <github email address>
2020-06-24 17:22:30 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
 
+    if self.end_with?('!')
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    delimiters = ['.', '!']
+    self.split(Regexp.union(delimiters)).length    #line credit https://stackoverflow.com/questions/19509307/split-string-by-multiple-delimiters
   end
 end
