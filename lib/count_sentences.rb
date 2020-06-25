f2a7f4b54778c6c00c7acbc8582f01aa54fdf557
“jzhouerm” <jzhouerm@hotmail.comgit config --global user.name jzhouermgit config --global user.email jzhouerm@hotmail.com>
2020-06-25 18:51:11 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    a = self.split(/\.|\?|\!/).reject {|space| space.empty?}
+    a.length
+    #binding.pry
+    # count = 0
+    # self.split(" ").each do |word|
+    #   if word.is_sentence? || 
   end
 end
+
+#"This, well, is a sentence. This is too!! And so is this, I think? Woo...".split(/((?<=[a-z0-9)][.?!])|(?<=[a-z0-9][.?!]"))\s+(?="?[A-Z])/)
